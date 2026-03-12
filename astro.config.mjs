bca57365981b7c226c06c9fb721db848daaddd3a
Walace Ramos <contatowalace.dev@gmail.com>
2026-03-12 10:19:18 -0300

    melhorando keywords e preparando para ferramentas google

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.erikaspinola.com.br',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()]
 });
